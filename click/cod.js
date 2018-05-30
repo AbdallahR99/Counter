37580c9f49d536b1082a4b45cc2e97f7efba2d96
Abdallah <abdallah-r99@hotmail.com>
2018-05-30 06:26:19 +0200

    update

--- click/cod.js
+++ click/cod.js
@@ -1,5 +1,7 @@
 /*global console*/
-var c = "#00f";
+var c = "#00f",
+    o,
+    p = parseFloat(o, 9);
 
 function clicked() {
     "use strict";
@@ -12,13 +14,16 @@ function clicked() {
         m = parseFloat(n, 10) + 1;
     z += 1;
     if (y.length === 1) {
+        o = document.getElementById("shadowOfNum").innerHTML;
         document.getElementById("num").innerHTML = "0" + z;
         document.getElementById("shadowOfNum").innerHTML = "0" + z;
     } else {
+        o = document.getElementById("shadowOfNum").innerHTML;
         document.getElementById("num").innerHTML = z;
         document.getElementById("shadowOfNum").innerHTML = z;
     }
     if (z === m) {
+        document.getElementById("shadowOfNum").innerHTML = o;
         document.getElementById("num").innerHTML = "00";
         document.getElementById("bg").style.background = "#00f";
         c = "#00f";
